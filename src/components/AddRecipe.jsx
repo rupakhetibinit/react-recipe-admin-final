f8beef42eed8350c9ffd46ee3d3c7a50919233af
Binit <cruiserponder@protonmail.com>
2022-04-26 16:10:52 +0545

    final commit

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { Flex, Input } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 const AddRecipe = () => {
-  return <div>AddRecipe</div>;
+  const [recipe, setRecipe] = useState({
+    name: '',
+    description: '',
+    imageUrl: '',
+  });
+  const [steps, setSteps] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  return (
+    <Flex flexDirection="column" padding="5" margin="10">
+      <h1>Add Recipe</h1>
+      <Input
+        type="text"
+        placeholder="Name"
+        onChange={e => setRecipe({ ...recipe, name: e.target.value })}
+      />
+    </Flex>
+  );
 };
 
 export default AddRecipe;
